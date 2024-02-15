aeb162e3a8f8420598186169ee30310f8103b372
오유진 <omh232323@gmail.com>
2024-02-15 21:13:38 +0900

    [lv1] 문자열 비교후 정렬

--- src/programmers/lv1/stringSort.js
+++ src/programmers/lv1/stringSort.js
@@ -1,7 +1,15 @@
 // link : "https://school.programmers.co.kr/learn/courses/30/lessons/12915"
 
 const solution = (strings, n) => {
-  return null;
+  strings.sort((a, b) => {
+    if (a[n] !== b[n]) {
+      return a[n] > b[n] ? 1 : -1;
+    }
+
+    return a > b ? 1 : -1;
+  });
+
+  return strings;
 };
 
 const strings = ["sun", "bed", "car"];
