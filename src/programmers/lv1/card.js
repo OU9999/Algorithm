0245e09d1854528cd2ca15b6d75f75fee81af6ff
오유진 <omh232323@gmail.com>
2024-03-15 22:29:22 +0900

    [lv1] 카드뭉치
    효율성 챙김

--- src/programmers/lv1/card.js
+++ src/programmers/lv1/card.js
@@ -1,10 +1,41 @@
 // link : https://school.programmers.co.kr/learn/courses/30/lessons/159994
 
 const solution = (cards1, cards2, goal) => {
-  return null;
+  let temp = [];
+  let endGame = true;
+  let goalCount = 0;
+
+  while (endGame) {
+    let goalItem = goal[goalCount];
+    let noResult = true;
+
+    if (cards1[0] === goalItem) {
+      temp.push(cards1[0]);
+      cards1.shift();
+      goalCount++;
+      noResult = false;
+    }
+
+    if (cards2[0] === goalItem) {
+      temp.push(cards2[0]);
+      cards2.shift();
+      goalCount++;
+      noResult = false;
+    }
+
+    if (noResult) {
+      return "No";
+    }
+
+    if (goalCount === goal.length) {
+      endGame = false;
+    }
+  }
+
+  return "Yes";
 };
 
-const cards1 = ["i", "drink", "water"];
+const cards1 = ["i", "water", "drink"];
 const cards2 = ["want", "to"];
 const goal = ["i", "want", "to", "drink", "water"];
 const result = solution(cards1, cards2, goal);
