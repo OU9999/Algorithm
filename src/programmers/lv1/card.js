dfc17766ac1555ba19ba0ad27c12eddd94b1f724
오유진 <omh232323@gmail.com>
2024-03-15 20:55:58 +0900

    [basic] dfs

---
+++ src/programmers/lv1/card.js
@@ -0,0 +1,11 @@
+// link : https://school.programmers.co.kr/learn/courses/30/lessons/159994
+
+const solution = (cards1, cards2, goal) => {
+  return null;
+};
+
+const cards1 = ["i", "drink", "water"];
+const cards2 = ["want", "to"];
+const goal = ["i", "want", "to", "drink", "water"];
+const result = solution(cards1, cards2, goal);
+console.log("RESULT >>>", result);
